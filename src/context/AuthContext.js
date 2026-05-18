48bee4fc6da7700aab24a2099350b8a030043449
GiannChryl <gianinacheryl04@gmail.com>
2026-05-19 03:32:12 +0700

    update lagiiiiii

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_API_URL || "http://localhost:5000";
 
 // Configure axios to send cookies with every request
 axios.defaults.withCredentials = true;
